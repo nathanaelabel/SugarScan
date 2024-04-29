c110c019f74b4b58e9ecbffcfb04ac101e11856d
Nathan <nathancodelab@gmail.com>
2024-04-29 10:55:06 +0700

    Initial Commit

---
+++ SugarScan/SugarScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugarScanApp.swift
+//  SugarScan
+//
+//  Created by Nathanael Abel on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SugarScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
